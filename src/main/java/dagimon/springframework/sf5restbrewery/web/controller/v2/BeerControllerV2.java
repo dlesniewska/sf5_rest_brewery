c260ff7b32e6c9cae78cdd830094cba25deb589f
dlesniewska <32817937+dlesniewska@users.noreply.github.com>
2019-12-27 14:10:58 +0100

    Lombok example with val and var features

--- src/main/java/dagimon/springframework/sf5restbrewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/dagimon/springframework/sf5restbrewery/web/controller/v2/BeerControllerV2.java
@@ -2,6 +2,9 @@ package dagimon.springframework.sf5restbrewery.web.controller.v2;
 
 import dagimon.springframework.sf5restbrewery.services.v2.BeerServiceV2;
 import dagimon.springframework.sf5restbrewery.web.model.v2.BeerDtoV2;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +18,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
+@RequiredArgsConstructor
 @Validated
 @RequestMapping("/api/v2/beer")
 @RestController
@@ -22,9 +27,9 @@ public class BeerControllerV2 {
 
     private final BeerServiceV2 beerService;
 
-    public BeerControllerV2(BeerServiceV2 beerService) {
-        this.beerService = beerService;
-    }
+//    public BeerControllerV2(BeerServiceV2 beerService) { //provided by lombok library
+//        this.beerService = beerService;
+//    }
 
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDtoV2> getBeer(@NotNull @PathVariable("beerId") UUID beerId) {
@@ -33,8 +38,12 @@ public class BeerControllerV2 {
 
     @PostMapping
     public ResponseEntity saveNewBeer(@Valid @RequestBody BeerDtoV2 beerDto) { //accepts a post of beerdto data in json
-        BeerDtoV2 savedBeerDto =  beerService.saveNewBeer(beerDto);
-        HttpHeaders responseHeaders = new HttpHeaders();
+        //BeerDtoV2 savedBeerDto =  beerService.saveNewBeer(beerDto); substituted by lombok val
+        log.debug("in handle post...");
+        val savedBeerDto = beerService.saveNewBeer(beerDto);
+
+        //HttpHeaders responseHeaders = new HttpHeaders(); //subsituted by lombok var
+        var responseHeaders = new HttpHeaders();
         responseHeaders.add("Location", "/api/v2/beer/" + savedBeerDto.getId().toString());
         return new ResponseEntity(responseHeaders, HttpStatus.CREATED);
     }
